e15fbdc35a805453db647a96ce817e99771ff0eb
Abdul Muyeed <abdulmuyeed47@gmail.com>
2023-04-24 23:25:17 +0600

    Update Function.java

--- LabAssignment/Function.java
+++ LabAssignment/Function.java
@@ -5,7 +5,7 @@ public class Function {
     
     public static void main(String[] args) {
         Clock myClock = new Clock();
-        System.out.println("My Time: " + myClock.getTime());
+        System.out.println("My  Time: " + myClock.getTime());
     
         Scanner scan = new Scanner(System.in);
         System.out.print("How many hours ahead is your timezone: ");
